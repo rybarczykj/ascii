a85fc756dabdfef5554c025b4ddcd7916351b65d
rybarczykj <jtrybarczyk@gmail.com>
2023-10-15 00:03:07 -0400

    add creatable select

--- src/PaletteDropdown.tsx
+++ src/PaletteDropdown.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import CreatableSelect from 'react-select/creatable';
+import Creatable, { useCreatable } from 'react-select/creatable';
 
 export const ASCIICHARS = [
     '8M0|*|::`,.',
@@ -10,6 +12,8 @@ export const ASCIICHARS = [
     '$@WgBMQNR8%0&đD#OGKEHdbmSqpAPwU54ZX96f23kVhaeFCj1IoJyst7}{YnulzriTx?][*Lcv×<>)(/+=÷“”!;:‘,’-.',
     ['8 ', 'M ', '0 ', '# ', '$ ', '| ', '* ', '+ ', ': ', ': ', '` ', '. ', '. '],
 ];
+
+const asciiOptions = ASCIICHARS.map((char) => ({ value: char, label: char }));
 
 interface PaletteDropdownProps {
     selectedOption: string | string[];
@@ -25,18 +29,30 @@ const PaletteDropdown: React.FC<PaletteDropdownProps> = ({ selectedOption, onOpt
     return (
         <div className="menu-entry">
             <label htmlFor="options">palette:</label>
-            <select
-                id="options"
-                value={selectedOption}
-                onChange={handleOptionChange}
-                className="dropdown">
-                <option value="">--Please choose an option--</option>
-                {ASCIICHARS.map((char, index) => (
-                    <option key={index} value={char}>
-                        {char}
-                    </option>
-                ))}
-            </select>
+            <CreatableSelect
+                options={asciiOptions}
+                className="dropdown"
+                onChange={(option) => onOptionChange(option?.value || ASCIICHARS[0])}
+                styles={{
+                    control: (baseStyles, state) => ({
+                        ...baseStyles,
+                        minHeight: '10px !important',
+                        // maxHeight: '10px !important',
+                        height: '30px',
+                        alignItems: 'baseline',
+                        width: state.menuIsOpen ? '200px' : '100px',
+                        margin: '0px auto',
+                        marginTop: '10px',
+                    }),
+                    dropdownIndicator: (baseStyles, state) => ({
+                        ...baseStyles,
+                        padding: '0px',
+                    }),
+                }}
+                components={{ IndicatorSeparator: () => null }}
+                menuPosition="fixed"
+                value={asciiOptions.find((option) => option.value === selectedOption)}
+            />
         </div>
     );
 };
